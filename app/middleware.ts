92238b398e7c3c2ca0d0b6b53852a31baa1a2f89
Berkay Derin <zewodi@gmail.com>
2024-06-05 19:11:51 +0300

    log middleware eklendi

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import { authMiddleware } from './middlewares/api/authMiddleware'
+import { logMiddleware } from './middlewares/api/logMiddleware'
 
 export const config = {
 	matcher: '/api/:path*'
@@ -7,6 +8,12 @@ export const config = {
 
 export default function middleware(request: Request) {
 	const authResult = authMiddleware(request)
+
+	if (request.url.includes('/api')) {
+		const logResult = logMiddleware(request)
+		console.log('LOG:', logResult.response)
+	}
+
 	if (!authResult?.isValid) {
 		return new NextResponse(JSON.stringify({ error: 'Unauthorized' }), {
 			status: 401,
